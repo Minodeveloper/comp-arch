c3e245ad938cf3945006ffc311720fd8ba9a96f4
Minodeveloper <minoshideveloper8@gmail.com>
2022-09-18 18:40:55 +0530

    exercise 15

---
+++ INTRO-COMP-ARCH/intro-to-comp/chapter_14/Text_Listings/sum9Ints_C/addNine.h
@@ -0,0 +1,8 @@
+/* addNine.h
+ * Returns sum of nine integers.
+ */
+#ifndef ADDNINE_H
+#define ADDNINE_H
+int addNine(int one, int two, int three, int four, int five,
+           int six, int seven, int eight, int nine);
+#endif
