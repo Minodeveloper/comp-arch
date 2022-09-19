3b396d5b48db88849e7b78f09b1a7be966336552
Minodeveloper <minoshideveloper8@gmail.com>
2022-09-19 22:29:20 +0530

    leanrt a bit of assembly

---
+++ INTRO-COMP-ARCH/ch10/doNothingProg.s
@@ -0,0 +1,14 @@
+	.file	"doNothingProg.c"
+	.intel_syntax noprefix
+	.text
+	.globl	main
+	.type	main, @function
+main:
+	push	rbp
+	mov	rbp, rsp
+	mov	eax, 0
+	pop	rbp
+	ret
+	.size	main, .-main
+	.ident	"GCC: (Ubuntu 11.2.0-19ubuntu1) 11.2.0"
+	.section	.note.GNU-stack,"",@progbits
